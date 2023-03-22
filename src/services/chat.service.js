0bcf89ad03accd6baac549bea69bce5357ad0567
MusicDev <smccowan33@gmail.com>
2023-03-21 23:45:10 -0700

    REady to launch!

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -4,34 +4,16 @@ The Chat Service will manage everything chat-related. This includes the followin
   - ????
 */
 import axios from 'axios';
-import { API_KEY } from "config";
-
-const reqConfig = {
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${API_KEY}`
-  }
-}
 
 const reqOpenAi = async (text) => {
-  const url = 'https://api.openai.com/v1/chat/completions';
-  let messages = [
-    {'role': 'user', 'content': 'You are a terse code completion machine. You will answer future questions with just code and nothing more.'},
-    {'role': 'assistant', 'content': 'Okay, I will answer your future questions with just code snippets. Let\'s get started!'}
-  ];
-
-  messages.push({
-    role: 'user',
-    content: text
-  });
+  const url = 'https://reads.shelbymccowan.com/api/v2/zokyo/code';
 
   const data = {
-    model: 'gpt-3.5-turbo',
-    messages
+    msg: text
   }
 
   try {
-    const res = await axios.post(url, data, reqConfig);
+    const res = await axios.post(url, data);
     res['success'] = true;
 
     return res
