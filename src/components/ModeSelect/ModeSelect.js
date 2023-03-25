7f59332bd33205e07cb61565baf17d80490a78ac
MusicDev <smccowan33@gmail.com>
2023-03-24 21:19:17 -0700

    Can now differentiate between modes

--- src/components/ModeSelect/ModeSelect.js
+++ src/components/ModeSelect/ModeSelect.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import './ModeSelect.scss';
 
 export const ModeSelect = ({ handle }) => {
+  const [currentMode, setMode] = useState('default');
+
+  const handleClick = (mode) => {
+    setMode(mode);
+    handle(mode);
+  }
+
   return (
     <div className='mode-select'>
-      <div className='mode' onClick={() => handle('default')}>
+      <div className={`mode ${currentMode === 'default' ? 'current' : ''}`} onClick={() => handleClick('default')}>
         Default
       </div>
 
-      <div className='mode' onClick={() => handle('code')}>
+      <div className={`mode ${currentMode === 'code' ? 'current' : ''}`} onClick={() => handleClick('code')}>
         Code
       </div>
     </div>
